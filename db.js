753b912965b5a288cc40ae3318a0cbb1e4a9346c
Poojitha Dabbara <dabbarapoojitha30@gmail.com>
2026-01-10 01:02:56 +0530

    updated

--- db.js
+++ db.js
@@ -1,17 +1,19 @@
-require('dotenv').config();
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
 });
 
 db.connect((err) => {
-  if (err) console.log("Database connection failed:", err);
-  else console.log("Database connected");
+  if (err) {
+    console.error("Database connection failed:", err);
+  } else {
+    console.log("Database connected successfully");
+  }
 });
 
 module.exports = db;
